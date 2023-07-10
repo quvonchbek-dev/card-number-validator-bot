cef386a0f6063ced81889235635187317588412b
Quvonchbek Umurzoqov <quvonchbek.dev@gmail.com>
2023-07-10 17:32:28 +0500

    First commit

---
+++ main.py
@@ -0,0 +1,48 @@
+from aiogram.types import Message
+from aiogram import Bot, Dispatcher, executor
+import asyncio
+from validator import validate_credit_card, format_number
+
+bot = Bot("6353520619:AAG_cEw60h0nLQByk6emDALVaj2nGA7kgVo", parse_mode="html")
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=["start"])
+async def start(msg: Message):
+    await msg.answer(
+        "Привет, добропожаловать. Пришлите мне номера карт. Один номер карты в строке. "
+        "Вы можете отправить столько строк, сколько хотите. Например:\n\n"
+        "<code>1234 5678 9012 3456\n4254-5478-9012-3756\n4244.5488.9012.3556</code>")
+
+
+@dp.message_handler(content_types=["text"])
+async def text_message(msg: Message):
+    new_msg = await bot.send_message(msg.chat.id, "⌛ Сейчас проверяю...")
+    tmp = msg.text
+    txt = ""
+    for i in tmp:
+        if i == "\n" or i.isdigit():
+            txt += i
+    x, y = 0, 0
+    h = ''
+    z = ""
+    clocks = "🕛🕧🕐🕜🕑🕝🕒🕞🕓🕟🕔🕠🕕🕡🕖🕢🕗🕣🕘🕤🕙🕥🕚🕦"
+
+    for i, card in enumerate(txt.split("\n")):
+        try:
+            valid = await validate_credit_card(card)
+            x += valid
+            y += not valid
+            fm = await format_number(card)
+            s = f"{'✅' if valid else '🚫'} {fm}\n"
+            h += s
+            z += s
+            await new_msg.edit_text(h + clocks[i % 24] + " проверка...")
+            # await asyncio.sleep(0.1)
+        except Exception as e:
+            await bot.send_message(msg.chat.id, f"⚠ Произошла ошибка в строке {i + 1}: <code>{e}</code>")
+    await new_msg.edit_text("☑ Результаты\n\n" + z)
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=False)
---
+++ validator.py
@@ -0,0 +1,31 @@
+import asyncio
+
+
+async def format_number(card_num):
+    res = ""
+    for i in range(len(card_num)):
+        res += card_num[i]
+        if i % 4 == 3 and i and i != len(card_num) - 1:
+            res += "-"
+    return res
+
+
+async def validate_credit_card(card_number: str) -> bool:
+    card_number = [int(num) for num in card_number]
+    check_igit = card_number.pop(-1)
+    card_number.reverse()
+    card_number = [num * 2 if idx % 2 == 0
+                   else num for idx, num in enumerate(card_number)]
+
+    card_number = [num - 9 if idx % 2 == 0 and num > 9
+                   else num for idx, num in enumerate(card_number)]
+    card_number.append(check_igit)
+    return sum(card_number) % 10 == 0
+
+
+async def main():
+    print(await format_number("1234567890123456"))
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
